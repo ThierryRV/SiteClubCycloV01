bab8329e08b697adf2b48d4bc513a7b8732158da
Thierry Herve <thierry_herve@yahoo.fr>
2017-11-16 18:55:16 +0100

    second commit

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,10 +1,10 @@
-import { SiteClubCycloPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('site-club-cyclo App', () => {
-  let page: SiteClubCycloPage;
+describe('test-ng App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new SiteClubCycloPage();
+    page = new AppPage();
   });
 
   it('should display welcome message', () => {
